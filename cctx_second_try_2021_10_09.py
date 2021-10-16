34585c6e99be668efdf6aa6bbec9049d29b92d3e
ichingq <default@deafult.ls>
2021-10-16 17:20:46 -0300

    fix

--- cctx_second_try_2021_10_09.py
+++ cctx_second_try_2021_10_09.py
@@ -8,28 +8,36 @@ print('CCXT Version:', ccxt.__version__)
 
 exchange = ccxt.binanceusdm()
 
-response = exchange.fetchOHLCV(
-    symbol='ADA/USDT',
-    timeframe='1h',
-    params={"price": 'index'}
-)
+# response = exchange.fetchOHLCV(
+#     symbol='ADA/USDT',
+#     timeframe='1h',
+#     params={"price": 'index'}
+# )
 
 #pprint(response)
 
 # Convenience methods --------------------------------------------------------
 
 markKlines = exchange.fetchMarkOHLCV(
-    symbol='BTC/USDT',
-    timeframe='5m',
+    symbol='ETH/USDT',
+    timeframe='1d',
     params={"price": 'mark'}
 )
 
-indexKlines = exchange.fetchIndexOHLCV(
-    symbol='BTC/USDT',
-    timeframe='5m',
-    params={"price": 'mark'}
-)
-pprint(markKlines)
+# indexKlines = exchange.fetchIndexOHLCV(
+#     symbol='BTC/USDT',
+#     timeframe='5m',
+#     params={"price": 'mark'}
+# )
+#pprint(markKlines)
+
+
+
+# exchange_info = exchange.get_exchange_info()
+# for s in exchange_info['symbols']:
+#     print(s['symbol'])
+
+
 dump = markKlines
 
 with open('cctx_dump.csv', 'w') as f:
